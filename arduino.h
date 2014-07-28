26672e55b190977143ebcc6b4a389e295779478c
Wilhelm Matilainen <wilhelm.matilainen@gmail.com>
2014-07-28 15:16:43 +0300

    Moved arduino stuff to arduino.h

---
+++ arduino.h
@@ -0,0 +1,69 @@
+#ifndef ARDUINO_H_NQZUPSUV
+#define ARDUINO_H_NQZUPSUV
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include <Arduino.h>
+
+template<const int pin>
+void write(uint8_t high)
+{
+	if (high)
+		TXLED1;
+	else
+		TXLED0;
+
+	digitalWrite(pin, high ? HIGH : LOW);
+}
+
+void delay_microseconds(size_t microseconds)
+{
+	delayMicroseconds(microseconds);
+}
+
+radio_time microseconds()
+{
+	return micros();
+}
+
+template<typename T, T *& radio>
+void on_receive()
+{
+	radio_on_receive(radio);
+}
+
+template<const int power_pin, const int data_pin>
+void transmitter_power(uint8_t on)
+{
+	pinMode(power_pin, OUTPUT);
+	pinMode(data_pin, OUTPUT);
+	digitalWrite(power_pin, on ? HIGH : LOW);
+}
+
+template<typename T, T *& radio, const int power_pin, const int data_pin>
+void receiver_power(uint8_t on)
+{
+	pinMode(power_pin, OUTPUT);
+	digitalWrite(power_pin, on ? HIGH : LOW);
+
+	if (on)
+		attachInterrupt(data_pin, on_receive<T, radio>, CHANGE);
+	else
+		detachInterrupt(data_pin);
+}
+
+template<const int power_pin>
+uint8_t powered()
+{
+	return digitalRead(power_pin) == HIGH ? 1 : 0;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // end of include guard: ARDUINO_H_NQZUPSUV
--- radio_arduino.h
+++ radio_arduino.h
@@ -6,63 +6,8 @@ extern "C" {
 #endif
 
 
-#include <Arduino.h>
-
+#include "arduino.h"
 #include "radio.h"
-
-
-template<const int pin>
-void write(uint8_t high)
-{
-	if (high)
-		TXLED1;
-	else
-		TXLED0;
-
-	digitalWrite(pin, high ? HIGH : LOW);
-}
-
-void delay_microseconds(size_t microseconds)
-{
-	delayMicroseconds(microseconds);
-}
-
-radio_time microseconds()
-{
-	return micros();
-}
-
-template<Radio *& radio>
-void on_receive()
-{
-	radio_on_receive(radio);
-}
-
-template<const int power_pin, const int data_pin>
-void transmitter_power(uint8_t on)
-{
-	pinMode(power_pin, OUTPUT);
-	pinMode(data_pin, OUTPUT);
-	digitalWrite(power_pin, on ? HIGH : LOW);
-}
-
-template<Radio *& radio, const int power_pin, const int data_pin>
-void receiver_power(uint8_t on)
-{
-	pinMode(power_pin, OUTPUT);
-	digitalWrite(power_pin, on ? HIGH : LOW);
-
-	if (on)
-		attachInterrupt(data_pin, on_receive<radio>, CHANGE);
-	else
-		detachInterrupt(data_pin);
-}
-
-template<const int power_pin>
-uint8_t powered()
-{
-	return digitalRead(power_pin) == HIGH ? 1 : 0;
-}
 
 template<Radio *& radio, const int transmitter_power_pin, const int transmitter_data_pin, const int receiver_power_pin, const int receiver_data_pin>
 void radio_create_arduino()
